3de11e8d6d92f43ccc5414ea9c71d4b8f3d86e4b
Ranielli Montagna <raniellimontagna@hotmail.com>
2024-12-13 14:15:44 -0300

    feat: :building_construction: add tailwind and router structure

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function Page() {
+  return (
+    <View className="flex flex-1">
+      <Text className="text-center text-2xl">Hello, world!</Text>
+    </View>
+  )
+}
