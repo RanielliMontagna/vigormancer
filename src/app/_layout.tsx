293150cf446a9ad3895540f57fad06e52dc59b1b
Ranielli Montagna <raniellimontagna@hotmail.com>
2024-12-21 18:38:30 -0300

    feat: add expo font and `lexend` assets

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import * as SplashScreen from 'expo-splash-screen'
 import Toast from 'react-native-toast-message'
+import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 
 import '@/styles/global.css'
@@ -18,9 +20,25 @@ if (!publishableKey) {
   )
 }
 
+SplashScreen.preventAutoHideAsync()
+
 export default function Layout() {
   const { isLoading } = useAppStore()
+
   const [isI18nInitialized, setIsI18nInitialized] = useState(false)
+
+  const [loaded, error] = useFonts({
+    'Lexend-Thin': require('@/assets/fonts/Lexend-Thin.ttf'),
+    'Lexend-Regular': require('@/assets/fonts/Lexend-Regular.ttf'),
+    'Lexend-Medium': require('@/assets/fonts/Lexend-Medium.ttf'),
+    'Lexend-Bold': require('@/assets/fonts/Lexend-Bold.ttf'),
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
 
   useEffect(() => {
     const initializeI18n = async () => {
@@ -31,7 +49,7 @@ export default function Layout() {
     initializeI18n()
   }, [])
 
-  if (!isI18nInitialized) {
+  if (!isI18nInitialized || (!loaded && !error)) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ActivityIndicator size="large" />
