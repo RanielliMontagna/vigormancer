e75e3e3c71db06571f029c37f0efcbd7b049de29
Ranielli Montagna <raniellimontagna@hotmail.com>
2025-01-07 09:13:36 -0300

    refactor: :recycle: add backdrop on bottom sheet component

--- src/components/ui/bottomSheet/bottomSheet.tsx
+++ src/components/ui/bottomSheet/bottomSheet.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
-import { BottomSheetModal, BottomSheetModalProps, BottomSheetView } from '@gorhom/bottom-sheet'
+import {
+  BottomSheetBackdrop,
+  BottomSheetModal,
+  BottomSheetModalProps,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet'
 
 const BottomSheetComponent = React.forwardRef<
   React.ElementRef<typeof BottomSheetModal>,
   BottomSheetModalProps
 >(({ children, ...rest }, ref) => {
+  const renderBackdrop = useCallback(
+    (props) => <BottomSheetBackdrop {...props} appearsOnIndex={1} animatedIndex={{ value: 1 }} />,
+    [],
+  )
+
   return (
-    <BottomSheetModal ref={ref} {...rest}>
+    <BottomSheetModal ref={ref} backdropComponent={renderBackdrop} {...rest}>
       <BottomSheetView>{children as React.ReactElement}</BottomSheetView>
     </BottomSheetModal>
   )
