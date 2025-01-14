144e9de632181965d5d1a0897601650471d9782c
Ranielli Montagna <raniellimontagna@hotmail.com>
2025-01-14 08:55:42 -0300

    feat: :sparkles: create sqlite provider and migrate function

--- src/app/(private)/_layout.tsx
+++ src/app/(private)/_layout.tsx
@@ -1,10 +1,15 @@
 import { Stack } from 'expo-router'
 import { View } from 'react-native'
 
+import { SQLiteProvider } from 'expo-sqlite'
+import { migrateDbIfNeeded } from '@/libs/db/db'
+
 export default function PrivateRoutesLayout() {
   return (
-    <View className="flex-1 bg-background">
-      <Stack screenOptions={{ headerShown: false, animation: 'slide_from_right' }} />
-    </View>
+    <SQLiteProvider databaseName="vigormancerdb" onInit={migrateDbIfNeeded}>
+      <View className="flex-1 bg-background">
+        <Stack screenOptions={{ headerShown: false, animation: 'slide_from_right' }} />
+      </View>
+    </SQLiteProvider>
   )
 }
---
+++ src/libs/db/db.ts
@@ -0,0 +1,53 @@
+import { SQLiteDatabase } from 'expo-sqlite'
+
+const DATABASE_VERSION = 1
+
+async function migrateDbIfNeeded(db: SQLiteDatabase) {
+  let { user_version: currentDbVersion } = await db.getFirstAsync<{ user_version: number }>(
+    'PRAGMA user_version',
+  )
+
+  // If the database is already up to date, return
+  if (currentDbVersion >= DATABASE_VERSION) return
+
+  if (currentDbVersion === 0) {
+    await db.execAsync(`
+      PRAGMA journal_mode = 'wal';
+      
+      -- Create workout table
+      CREATE TABLE IF NOT EXISTS workouts (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          name TEXT NOT NULL,
+          description TEXT,
+          date DATE DEFAULT (DATE('now')),
+          image_path TEXT
+      );
+
+      -- Create exercise table
+      CREATE TABLE IF NOT EXISTS exercises (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          workout_id INTEGER NOT NULL,
+          name TEXT NOT NULL,
+          type TEXT NOT NULL,
+          repetitions INTEGER,
+          sets INTEGER,
+          distance REAL,
+          duration INTEGER,
+          rest_time INTEGER,
+          FOREIGN KEY (workout_id) REFERENCES workouts (id) ON DELETE CASCADE
+      );
+    `)
+
+    currentDbVersion = DATABASE_VERSION
+  }
+
+  await db.execAsync(`PRAGMA user_version = ${DATABASE_VERSION}`)
+
+  if (currentDbVersion > 0) {
+    console.log(`Database migrated to version ${DATABASE_VERSION}`)
+  } else {
+    console.log(`Database initialized at version ${DATABASE_VERSION}`)
+  }
+}
+
+export { migrateDbIfNeeded }
