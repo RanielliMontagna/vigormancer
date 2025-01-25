61c0106c4cc34e171b5260109d594f7d1c9a60fc
Ranielli Montagna <raniellimontagna@hotmail.com>
2025-01-24 21:35:16 -0300

    refactor: :recycle: adjust queries of exercises repository

--- src/db/repositories/exercises.ts
+++ src/db/repositories/exercises.ts
@@ -5,6 +5,7 @@ export enum ExerciseType {
 
 export interface Exercise {
   id: string
+  categoryId: string
   name: string
   type: ExerciseType
   image?: string
@@ -12,13 +13,18 @@ export interface Exercise {
   updatedAt: string
 }
 
+export interface ExerciseWithCategory
+  extends Omit<Exercise, 'categoryId' | 'createdAt' | 'updatedAt'> {
+  category: { id: string; name: string }
+}
+
 export type CreateExerciseParams = Omit<Exercise, 'id' | 'createdAt' | 'updatedAt'>
-export type UpdateExerciseParams = Omit<Exercise, 'createdAt' | 'updatedAt'>
+export type UpdateExerciseParams = Omit<Exercise, 'categoryId' | 'createdAt' | 'updatedAt'>
 
 export interface ExercisesRepository {
   createExercise(exercise: CreateExerciseParams): Promise<{ id: string }>
-  getExercise(id: string): Promise<Exercise | undefined>
-  getExercises(): Promise<Exercise[]>
+  getExercise(id: string): Promise<ExerciseWithCategory | undefined>
+  getExercises(): Promise<ExerciseWithCategory[]>
   updateExercise(exercise: UpdateExerciseParams): Promise<void>
   deleteExercise(id: string): Promise<void>
 }
--- src/db/repositories/sqlite/sqlite-exercises-repository.ts
+++ src/db/repositories/sqlite/sqlite-exercises-repository.ts
@@ -10,24 +10,62 @@ import { db } from '@/db'
 
 export class SqliteExercisesRepository implements ExercisesRepository {
   async getExercises() {
-    const exercises = await db.getAllAsync<Exercise>('SELECT * FROM exercises')
+    const exercises = await db.getAllAsync<Exercise & { categoryName: string; categoryId: string }>(
+      `SELECT exercises.*, categories.name as categoryName, categories.id as categoryId
+       FROM exercises 
+       JOIN categories ON exercises.categoryId = categories.id`,
+    )
 
-    return exercises
+    return exercises.map((exercise) => ({
+      id: exercise.id,
+      name: exercise.name,
+      type: exercise.type,
+      image: exercise.image,
+      category: { id: exercise.categoryId, name: exercise.categoryName },
+    }))
   }
 
   async getExercise(id: string) {
-    const exercise = await db.getFirstAsync<Exercise>('SELECT * FROM exercises WHERE id = ?', [id])
+    const exercise = await db.getFirstAsync<
+      Exercise & { categoryName: string; categoryId: string }
+    >(
+      `SELECT exercises.*, categories.name as categoryName, categories.id as categoryId
+       FROM exercises 
+       JOIN categories ON exercises.categoryId = categories.id 
+       WHERE exercises.id = ?`,
+      [id],
+    )
 
-    return exercise
+    if (!exercise) {
+      throw new Error('Exercise not found')
+    }
+
+    return {
+      id: exercise.id,
+      name: exercise.name,
+      type: exercise.type,
+      image: exercise.image,
+      category: { id: exercise.categoryId, name: exercise.categoryName },
+    }
   }
 
   async createExercise(exercise: CreateExerciseParams) {
     const id = uuidv4()
 
+    const category = await db.getFirstAsync<{ id: string }>(
+      'SELECT id FROM categories WHERE id = ?',
+      [exercise.categoryId],
+    )
+
+    if (!category) {
+      throw new Error('Category not found')
+    }
+
     await db.runAsync(
-      'INSERT INTO exercises (id, name, type, image, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ?)',
+      'INSERT INTO exercises (id, categoryId, name, type, image, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?)',
       [
         id,
+        category.id,
         exercise.name,
         exercise.type,
         exercise.image,
