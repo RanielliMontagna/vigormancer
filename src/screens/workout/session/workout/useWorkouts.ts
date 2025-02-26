b4971cd1bc82065599ebef52737fa6202b24d92d
Ranielli Montagna <raniellimontagna@hotmail.com>
2025-02-26 07:53:04 -0300

    feat: :sparkles: create rest time screen

--- src/screens/workout/session/workout/useWorkouts.ts
+++ src/screens/workout/session/workout/useWorkouts.ts
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useSessionContext } from '../session.context'
+import { WorkoutDifficulty } from '@/db/repositories/workouts'
+import colors from 'tailwindcss/colors'
 
 export function useWorkout() {
   const { workout, nextStep } = useSessionContext()
 
-  console.log(workout.exercises)
+  const secondsBetweenExercises = 30
+  const additionalRestTime = 10
 
+  const [secondsToRest, setSecondsToRest] = useState(secondsBetweenExercises)
   const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0)
+  const [restTime, setRestTime] = useState(0)
 
   function handleGoToNextExercise() {
     if (currentExerciseIndex === workout.exercises.length - 1) {
@@ -14,13 +19,77 @@ export function useWorkout() {
       return
     }
 
+    setRestTime(secondsToRest)
     setCurrentExerciseIndex((prev) => prev + 1)
   }
 
+  function handleGoToPreviousExercise() {
+    if (currentExerciseIndex === 0) return
+
+    setCurrentExerciseIndex((prev) => prev - 1)
+  }
+
+  function handleAddRestTime() {
+    setRestTime((prev) => prev + additionalRestTime)
+    setSecondsToRest((prev) => prev + additionalRestTime)
+  }
+
+  function handleSkipRestTime() {
+    setRestTime(0)
+  }
+
+  function handleDoneCurrentExercise() {
+    handleGoToNextExercise()
+  }
+
+  function handleSkipCurrentExercise() {
+    handleGoToNextExercise()
+  }
+
+  const difficultyColor = useMemo(() => {
+    switch (workout?.difficulty) {
+      default:
+      case WorkoutDifficulty.BEGINNER:
+        return colors.indigo[400]
+      case WorkoutDifficulty.INTERMEDIATE:
+        return colors.indigo[600]
+      case WorkoutDifficulty.ADVANCED:
+        return colors.indigo[800]
+    }
+  }, [workout])
+
+  const fillCountdown = useMemo(
+    () => ((secondsToRest - restTime) / secondsToRest) * 100,
+    [restTime, secondsToRest],
+  )
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRestTime((prev) => prev - 1)
+    }, 1000)
+
+    return () => clearInterval(interval)
+  })
+
+  useEffect(() => {
+    if (restTime === 0) {
+      setSecondsToRest(secondsBetweenExercises)
+    }
+  }, [restTime])
+
   return {
+    restTime,
+    fillCountdown,
+    difficultyColor,
+    additionalRestTime,
     currentExerciseIndex,
+    exerciseCount: workout.exercises.length,
     activeExercise: workout.exercises[currentExerciseIndex],
     nextStep,
-    handleGoToNextExercise,
+    handleAddRestTime,
+    handleSkipRestTime,
+    handleDoneCurrentExercise,
+    handleSkipCurrentExercise,
+    handleGoToPreviousExercise,
   }
 }
--- src/screens/workout/session/workout/workout.tsx
+++ src/screens/workout/session/workout/workout.tsx
@@ -9,13 +9,88 @@ import { Exit } from '../exit/exit'
 import { useWorkout } from './useWorkouts'
 import { Button, Text } from '@/components'
 import { useColorScheme } from '@/hooks'
+import { AnimatedCircularProgress } from 'react-native-circular-progress'
+import colors from 'tailwindcss/colors'
 
 export function Workout() {
   const { t } = useTranslation()
   const { isDarkColorScheme } = useColorScheme()
-  const { currentExerciseIndex, activeExercise } = useWorkout()
+
+  const {
+    restTime,
+    exerciseCount,
+    fillCountdown,
+    activeExercise,
+    difficultyColor,
+    additionalRestTime,
+    currentExerciseIndex,
+    handleAddRestTime,
+    handleSkipRestTime,
+    handleDoneCurrentExercise,
+    handleSkipCurrentExercise,
+    handleGoToPreviousExercise,
+  } = useWorkout()
 
   const asset = Asset.fromModule(exerciseImageMap[`${activeExercise.exerciseName}.jpg`])
+
+  if (restTime > 0) {
+    return (
+      <View className="gap-4 flex-col flex-1">
+        <Exit />
+        <Image source={{ uri: asset.uri }} className="w-full h-56 rounded-lg bg-zinc-200" />
+        <View className="p-4 bg-card rounded-lg gap-4">
+          <View>
+            <Text className="text-sm font-regular">
+              {t('workout.workoutDetails.exerciseCard.next')} {currentExerciseIndex + 1}/
+              {exerciseCount}
+            </Text>
+            <Text className="text-lg font-bold">{t(`${activeExercise.exerciseName}.title`)}</Text>
+            <Text className="text-sm text-gray-500">
+              {t('workout.workoutDetails.exerciseCard.sets', { sets: activeExercise.sets })}
+              {t('workout.workoutDetails.exerciseCard.reps', { reps: activeExercise.repetitions })}
+            </Text>
+          </View>
+        </View>
+        <View
+          testID="countdown-container"
+          className="flex-1 flex-column justify-center items-center gap-4"
+        >
+          <Text className="text-xs">{t('workout.workoutDetails.exerciseCard.countdown')}</Text>
+          <AnimatedCircularProgress
+            size={150}
+            width={10}
+            fill={fillCountdown}
+            fillLineCap="round"
+            rotation={0}
+            tintColor={difficultyColor}
+            backgroundColor={isDarkColorScheme ? colors.gray[600] : colors.gray[200]}
+          >
+            {() => {
+              return <Text className="text-4xl font-lexend-semibold">{restTime}</Text>
+            }}
+          </AnimatedCircularProgress>
+        </View>
+        <View className="flex-row justify-between gap-4">
+          <Button onPress={handleAddRestTime} variant="secondary" className="flex-1">
+            <Text>
+              {t('workout.workoutDetails.exerciseCard.addRestTime', {
+                seconds: additionalRestTime?.toString(),
+              })}
+            </Text>
+          </Button>
+          <View id="divider" className="w-0.5 h-10 bg-gray-200" />
+          <Button
+            onPress={handleSkipRestTime}
+            variant="secondary"
+            className="flex-1"
+            startIcon={<FontAwesome6 name="play" />}
+          >
+            <Text>{t('workout.workoutDetails.exerciseCard.startNow')}</Text>
+          </Button>
+        </View>
+      </View>
+    )
+  }
 
   return (
     <View className="gap-4 flex-col flex-1">
@@ -41,7 +116,7 @@ export function Workout() {
         <Button
           className="w-full"
           size="lg"
-          onPress={() => {}}
+          onPress={handleDoneCurrentExercise}
           startIcon={<FontAwesome6 name="check" color={isDarkColorScheme ? 'black' : 'white'} />}
         >
           <Text>{t('workout.workoutDetails.exerciseCard.done')}</Text>
@@ -49,7 +124,7 @@ export function Workout() {
       </View>
       <View className="flex-row justify-between gap-4">
         <Button
-          onPress={() => {}}
+          onPress={handleGoToPreviousExercise}
           variant="secondary"
           className="flex-1"
           disabled={currentExerciseIndex === 0}
@@ -59,7 +134,7 @@ export function Workout() {
         </Button>
         <View id="divider" className="w-0.5 h-10 bg-gray-200" />
         <Button
-          onPress={() => {}}
+          onPress={handleSkipCurrentExercise}
           variant="secondary"
           className="flex-1"
           startIcon={<FontAwesome6 name="arrow-right" />}
